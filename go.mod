7cace136cb708831881c3e43811f649414b9a4e9
Paul Miller <pmiller@microsoft.com>
2023-10-14 22:54:28 +0000

    okay dump a yaml list

--- go.mod
+++ go.mod
@@ -3,15 +3,20 @@ module github.com/paulgmiller/wg-sync
 go 1.21.1
 
 require (
+	github.com/BurntSushi/toml v1.3.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/josharian/native v1.1.0 // indirect
 	github.com/mdlayher/genetlink v1.3.2 // indirect
 	github.com/mdlayher/netlink v1.7.2 // indirect
 	github.com/mdlayher/socket v0.4.1 // indirect
+	github.com/rb-go/namegen v1.1.0 // indirect
+	github.com/samber/lo v1.38.1 // indirect
 	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	golang.zx2c4.com/wireguard v0.0.0-20230325221338-052af4a8072b // indirect
 	golang.zx2c4.com/wireguard/wgctrl v0.0.0-20230429144221-925a1e7659e6 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,35 @@
 package main
 
 import (
+	"encoding/base64"
+	"fmt"
 	"log"
+	"net"
+	"os"
+	"strings"
 
+	"github.com/rb-go/namegen"
+	"github.com/samber/lo"
 	"golang.zx2c4.com/wireguard/wgctrl"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+	"gopkg.in/yaml.v2"
 )
 
+type prettyPeer struct {
+	PublicKey  string `yaml:"PublicKey,omitempty"`
+	AllowedIPs string `yaml:"AllowedIPs,omitempty"`
+	Endpoint   string `yaml:"Endpoint,omitempty"`
+	//KeepAlive
+}
+
+func PrettyPeer(p wgtypes.Peer) prettyPeer {
+	return prettyPeer{
+		PublicKey:  base64.StdEncoding.EncodeToString(p.PublicKey[:]),
+		AllowedIPs: strings.Join(lo.Map(p.AllowedIPs, func(item net.IPNet, _ int) string { return item.String() }), ","),
+		//how do we know if these are public or temporary? is it fine to guess?
+		//Endpoint: p.Endpoint.String(),
+	}
+}
 func main() {
 	wg, err := wgctrl.New()
 	if err != nil {
@@ -16,11 +40,65 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	for _, d := range devices {
-		log.Printf("%s %s %s\n", d.Name, d.PublicKey, d.ListenPort)
-		for _, p := range d.Peers {
-			log.Printf("  %s %s %s\n", p.PublicKey, p.AllowedIPs, p.Endpoint)
-		}
+
+	if len(devices) == 0 {
+		log.Fatal("no wireguard devices found")
 	}
 
+	if len(devices) != 1 {
+		log.Fatal("multiple devices: TODO specify one as arg")
+	}
+
+	d0 := devices[0]
+
+	me := prettyPeer{
+		PublicKey:  d0.PublicKey.String(),
+		AllowedIPs: GetWireGaurdIP(d0.Name),
+		Endpoint:   fmt.Sprintf("%s:%d", GetOutboundIP().String(), d0.ListenPort),
+	}
+
+	peers := map[string]prettyPeer{}
+	peers[namegen.GetName(0)] = me
+
+	for _, peer := range devices[0].Peers {
+		peers[namegen.GetName(0)] = PrettyPeer(peer)
+	}
+
+	stdout := yaml.NewEncoder(os.Stdout)
+	err = stdout.Encode(peers)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func GetWireGaurdIP(interfacename string) string {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		log.Fatalf("can't get interfaces: %v", err)
+	}
+
+	wginterface, found := lo.Find(ifaces, func(iface net.Interface) bool { return iface.Name == interfacename })
+	if !found {
+		log.Fatalf("can't get interfaces: %v", err)
+	}
+
+	addrs, err := wginterface.Addrs()
+	if err != nil {
+		log.Fatalf("can't get interface addrs: %v", err)
+	}
+	return addrs[0].String()
+}
+
+// Get preferred outbound ip of this machine
+func GetOutboundIP() net.IP {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+
+	return localAddr.IP
 }
