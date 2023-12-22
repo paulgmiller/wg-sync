e356ea70f9ec1420f5e53883e8fd728635799135
Paul Miller (AWESOME) <pmiller@microsoft.com>
2023-12-22 14:01:43 -0800

    simple ini writer

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.1
 
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/native v1.1.0 // indirect
--- pretty/peers.go
+++ pretty/peers.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"strings"
 
+	"github.com/go-ini/ini"
 	"github.com/samber/lo"
 	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 	"gopkg.in/yaml.v2"
@@ -15,7 +16,7 @@ type Peer struct {
 	PublicKey  string `yaml:"PublicKey,omitempty"`
 	AllowedIPs string `yaml:"AllowedIPs,omitempty"`
 	Endpoint   string `yaml:"Endpoint,omitempty"`
-	Server     bool   `yaml:"Server,omitempty"`
+	//Server     bool   `yaml:"Server,omitempty"`
 }
 
 //lets try https://raw.githubusercontent.com/paulgmiller/paulgmiller.github.io/master/peers.yaml
@@ -27,6 +28,24 @@ func New(p wgtypes.Peer) Peer {
 		//how do we know if these are public or temporary? is it fine to guess?
 		//Endpoint: p.Endpoint.String(),
 	}
+}
+
+func Ini(w io.Writer, peers ...Peer) error {
+	iniFile := ini.Empty()
+	for _, p := range peers {
+		sec, err := iniFile.NewSection("PEER")
+		if err != nil {
+			return err
+		}
+		//tecnically new key returns an err
+		sec.NewKey("PublicKey", p.PublicKey)
+		sec.NewKey("AllowedIPs", p.AllowedIPs)
+		if p.Endpoint != "" {
+			sec.NewKey("AllowedIPs", p.AllowedIPs)
+		}
+	}
+	_, err := iniFile.WriteTo(w)
+	return err
 }
 
 func Yaml(w io.Writer, peers ...wgtypes.Peer) error {
